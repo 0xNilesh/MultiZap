cab039f99d156d97cb9dec251152efb16ac94168
Nilesh Gupta <guptanilesh2312@gmail.com>
2025-08-02 12:42:57 +0530

    refactor: added script for deployment in evm_htlc

--- evm_htlc/script/deploy.s.sol
+++ evm_htlc/script/deploy.s.sol
@@ -3,15 +3,19 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {HTLCEscrowFactory} from "../src/HTLCEscrowFactory.sol";
+import {HTLCEscrow} from "../src/HTLCEscrow.sol";
+import {USDC} from "../src/mocks/MockERC20.sol";
 
 contract HTLCEscrowFactoryScript is Script {
     HTLCEscrowFactory public htlcEscrowFactory;
+    USDC public usdcToken;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
+        usdcToken = new USDC("USDC", "USDC", 18);
         htlcEscrowFactory = new HTLCEscrowFactory();
 
         vm.stopBroadcast();
